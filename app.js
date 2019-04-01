cca3d8afa76754093c2e1147640e8e9c41dd4837
pepcilia <pepcilia@donga.ac.kr>
2019-04-01 04:00:43 -0400

    MVC pattern part 2 /I understood routes

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ import morgan from "morgan"; // logger middleware
 import helmet from "helmet" // security for nodeJS
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import {userRouter} from "./routes";
 
 const app = express();
-
-const PORT = 3000;
-
-const handleListening = () => console.log(`Listening on : ${PORT}`);
 
 const handleHome = (req, res) => res.send("HI Home.");
 
@@ -17,7 +14,9 @@ const handleProfile = (req, res) => res.send("HI Profile");
 
 // globally middlewares ex) ip ban... check log..
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(helmet());
 app.use(morgan("dev"));
 //--------------------------------
@@ -26,4 +25,6 @@ app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(3000, handleListening);
+app.use("/user", userRouter);
+
+export default app;
