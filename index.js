1e53991100a7894db8911bc0f768b59574b40f48
pepcilia <pepcilia@donga.ac.kr>
2019-04-01 03:23:42 -0400

    middlewares Part1

--- index.js
+++ index.js
@@ -8,10 +8,17 @@ const handleListening = () => console.log(`Listening on : ${PORT}`);
 
 const handleHome = (req, res) => res.send("HI Home.");
 
+const betweenHome = (req, res, next) => {
+  console.log("I'm between");
+  next();
+}
+
 const handleProfile = (req, res) => res.send("HI Profile");
 
-
-app.get("/",handleHome);
-app.get("/profile",handleProfile);
+// globally middlewares ex) ip ban... check log..
+app.use(betweenHome);
+//--------------------------------
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
 
 app.listen(3000, handleListening);
--- studing.md
+++ studing.md
@@ -1,3 +1,4 @@
 [+] githube.com Test
 [+] npm init and npm install usage
-[]
+[+] babel(@babel/core , @babel/node , @babel/preset-env) and nodemon
+[+] express에서는 모든 함수가 미들웨어가 될 수 있다.
