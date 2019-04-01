7f33fb7ee6510623edeee745e089b948bd47a357
pepcilia <pepcilia@donga.ac.kr>
2019-04-01 04:40:35 -0400

    MVC pattern before controller parts

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,5 +1,18 @@
 import express from "express";
+import routes from "../routes";
 
 const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+const handleJoin = (req, res) => res.send("Join");
+const handleLogin = (req, res) => res.send("Login");
+const handleLogout = (req, res) => res.send("Logout");
+const handleSearch = (req, res) => res.send("Search");
+
+globalRouter.get(routes.home, handleHome);
+globalRouter.get(routes.join, handleJoin);
+globalRouter.get(routes.login, handleLogin);
+globalRouter.get(routes.logout, handleLogout);
+globalRouter.get(routes.search, handleSearch);
 
 export default globalRouter;
