b33c7812dbe644c12e438744eccf5a611c18b1f3
pepcilia <pepcilia@donga.ac.kr>
2019-04-01 03:35:29 -0400

    add middlewares (morgan, helmet, body-parser, cookie-parser) and understand those

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan"; // logger middleware
+import helmet from "helmet" // security for nodeJS
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -8,17 +12,18 @@ const handleListening = () => console.log(`Listening on : ${PORT}`);
 
 const handleHome = (req, res) => res.send("HI Home.");
 
-const betweenHome = (req, res, next) => {
-  console.log("I'm between");
-  next();
-}
 
 const handleProfile = (req, res) => res.send("HI Profile");
 
 // globally middlewares ex) ip ban... check log..
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(helmet());
+app.use(morgan("dev"));
 //--------------------------------
+
 app.get("/", handleHome);
+
 app.get("/profile", handleProfile);
 
 app.listen(3000, handleListening);
--- studing.md
+++ studing.md
@@ -2,3 +2,4 @@
 [+] npm init and npm install usage
 [+] babel(@babel/core , @babel/node , @babel/preset-env) and nodemon
 [+] express에서는 모든 함수가 미들웨어가 될 수 있다.
+[+]middleware에서 res.send와 같은 response를 route가 처리되기 전에 보낸다면 연결을 차단할 수 있음.
