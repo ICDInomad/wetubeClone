c416538dc39cb309ef29f26767580d947d3bc19f
pepcilia <pepcilia@donga.ac.kr>
2019-04-03 06:16:58 -0400

    pug variables study

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import morgan from "morgan"; // logger middleware
 import helmet from "helmet" // security for nodeJS
-import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 
 const app = express();
+app.use(helmet());
 app.set('view engine', "pug");
 // globally middlewares ex) ip ban... check log..
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localsMiddleware)
 //--------------------------------
-
 app.use(routes.home, globalRouter)
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
