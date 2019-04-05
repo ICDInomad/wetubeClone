09742aca8ae97f66dc6169387d6584ed823f528e
pepcilia <pepcilia@donga.ac.kr>
2019-04-05 07:22:13 -0400

    add some pug files, and make home controller with fake DB

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 324393,
+        title: "Video awesome",
+        description: "This is something",
+        views: 24,
+        videoFile: "https://www.youtube.com/watch?v=RWRO1bVhU2g",
+        creator: {
+            id:"121212",
+            name:"Armenpo",
+            email: "pepcilia@donga.ac.kr"
+        }
+    },
+        {
+        id: 11111,
+        title: "Video good",
+        description: "This is something",
+        views: 24,
+        videoFile: "https://www.youtube.com/watch?v=RWRO1bVhU2g",
+        creator: {
+            id:"121212",
+            name:"Armenpo",
+            email: "pepcilia@donga.ac.kr"
+        }
+    },
+        {
+        id: 22222,
+        title: "Video nice",
+        description: "This is something",
+        views: 24,
+        videoFile: "https://www.youtube.com/watch?v=RWRO1bVhU2g",
+        creator: {
+            id:"121212",
+            name:"Armenpo",
+            email: "pepcilia@donga.ac.kr"
+        }
+    },
+        {
+        id: 33333,
+        title: "Video weeeeee",
+        description: "This is something",
+        views: 24,
+        videoFile: "https://www.youtube.com/watch?v=RWRO1bVhU2g",
+        creator: {
+            id:"121212",
+            name:"Armenpo",
+            email: "pepcilia@donga.ac.kr"
+        }
+    }
+]
