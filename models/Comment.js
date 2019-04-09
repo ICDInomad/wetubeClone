a151c812e914110ce162ce55a9ec7f75527b06bd
pepcilia <pepcilia@donga.ac.kr>
2019-04-09 06:55:58 -0400

    comment Schema

---
+++ models/Comment.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const CommentSchema = mongoose.Schema({
+    text:{
+        type: String,
+        required: "Text is requied"
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    },
+    video: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video"
+    }
+});
